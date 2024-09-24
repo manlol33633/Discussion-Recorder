7c1b118ee94705442dda1ae2be10fc0e2664907c
manlol33633 <51886476+manlol33633@users.noreply.github.com>
2024-09-23 20:41:42 -0500

    Main Discussion

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-document.addEventListener('mousemove', function(event) {
-    var x = event.clientX;
-    var y = event.clientY;
-    var person = document.getElementById('person');
-    if (x > 100 && x < 200 && y > 75 && y < 400) {
-        person.setAttribute('transform', 'translate(5, 0) scale(1.1)');
-    } else {
-        person.setAttribute('transform', 'translate(0, 0) scale(1)');
-    }
-});
+var setupInfo = localStorage.getItem('setupInfo');
 
+// print(setupInfo['x']);
+// print(setupInfo['y']);
+
+var symmetrical = true; // If the number of seats on each opposite side are symmetrical
+var x = 5; // Placeholder for horizontal seats
+var y = 10; // Placeholder for vertical seats
+
+// var x_top = 5; // Placeholder for horizontal seats on the top
+// var x_bottom = 6; // Placeholder for horizontal seats on the bottom
+// var y_left = 4; // Placeholder for vertical seats on the left
+// var y_right = 5; // Placeholder for vertical seats on the right
+
+var baseSize = 50;
+
+console.log(window.innerWidth + " " + window.innerHeight);
---
+++ setup_info.js
@@ -0,0 +1,6 @@
+var setupInfo = {
+    'x' : 5, 
+    'y' : 10
+};
+
+localStorage.setItem('setupInfo', setupInfo);
